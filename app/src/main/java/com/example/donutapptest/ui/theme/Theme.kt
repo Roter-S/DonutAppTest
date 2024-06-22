7797d1b4a6f7449a03b2214546ed546a29cacc8d
Roter-S <bryan.sosof@gmail.com>
2024-06-22 14:01:40 -0600

    feat: login screan
    - Main logic for login
    - Creation of components for login such as username and password
    - Byte logo upload
    - Resource strings creation

--- app/src/main/java/com/example/donutapptest/ui/theme/Theme.kt
+++ app/src/main/java/com/example/donutapptest/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.donutapptest.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,15 +11,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = SkyBlueDark,
+    secondary = BlueDarker,
+    tertiary = DeepBlue,
+    error = ErrorRed,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = SkyBlue,
+    secondary = BlueDark,
+    tertiary = LightBlue,
+    error = ErrorRed,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
