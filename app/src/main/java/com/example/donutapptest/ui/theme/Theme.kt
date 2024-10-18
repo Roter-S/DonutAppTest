883bebd9fb11de4b58dac8b34782adea5e336fc8
Roter-S <bryan.sosof@gmail.com>
2024-10-18 16:34:36 -0600

    feature: color palette

--- app/src/main/java/com/example/donutapptest/ui/theme/Theme.kt
+++ app/src/main/java/com/example/donutapptest/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.donutapptest.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,28 +8,47 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
+val BluePrimary = Color(0xFF2699FB)  // Primary Color
+val DarkBlueSecondary = Color(0xFF0A2459)  // Secondary Color
+
+val LightBlueTertiary = Color(0xFF82CFFF)  // Tertiary Color, un tono más claro de azul
+val DarkerPrimary = Color(0xFF1A7DC0)  // Versión más oscura del primary
+val LighterSecondary = Color(0xFF123972)  // Versión más clara del secondary
+val BackgroundColor = Color(0xFFF5F9FF)  // Fondo, basado en un tono claro y neutro
+val SurfaceColor = Color(0xFFFFFFFF)  // Superficies en blanco
+val OnPrimaryColor = Color(0xFFFFFFFF)  // Texto sobre primary
+val OnSecondaryColor = Color(0xFFFFFFFF)  // Texto sobre secondary
+val OnTertiaryColor = Color(0xFF000000)  // Texto sobre terciary (negro para contraste)
+val OnBackgroundColor = Color(0xFF0A2459)  // Texto sobre el fondo
+val OnSurfaceColor = Color(0xFF0A2459)  // Texto sobre superficies
+
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BluePrimary,
+    secondary = DarkBlueSecondary,
+    tertiary = LightBlueTertiary,
+    background = DarkBlueSecondary,
+    surface = DarkerPrimary,
+    onPrimary = OnPrimaryColor,
+    onSecondary = OnSecondaryColor,
+    onTertiary = OnTertiaryColor,
+    onBackground = OnBackgroundColor,
+    onSurface = OnSurfaceColor
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = BluePrimary,
+    secondary = DarkBlueSecondary,
+    tertiary = LightBlueTertiary,
+    background = BackgroundColor,
+    surface = SurfaceColor,
+    onPrimary = OnPrimaryColor,
+    onSecondary = OnSecondaryColor,
+    onTertiary = OnTertiaryColor,
+    onBackground = OnBackgroundColor,
+    onSurface = OnSurfaceColor
 )
 
 @Composable
