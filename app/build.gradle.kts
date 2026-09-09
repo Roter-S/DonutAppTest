449061cab963a0ff58c8af8263b60e51d153a8ce
Roter-S <bryan.sosof@gmail.com>
2026-09-08 23:44:37 -0600

    build: update gradle wrapper, build configuration and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,12 +54,8 @@ dependencies {
 
     // room
     implementation(libs.androidx.room.runtime)
-    annotationProcessor(libs.androidx.room.room.compiler)
     ksp(libs.androidx.room.room.compiler)
     implementation(libs.androidx.room.ktx)
-
-    // threetenabp
-    implementation(libs.threetenabp)
 
     // retrofit
     implementation(libs.retrofit)
@@ -81,20 +77,24 @@ dependencies {
     // datastore
     implementation(libs.androidx.datastore.preferences)
 
+    // Material 3
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.window.size.class1)
+    implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
+
     // compose
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
--- app/src/main/java/com/example/donutapptest/ui/navigation/NavigationComponent.kt
+++ app/src/main/java/com/example/donutapptest/ui/navigation/NavigationComponent.kt
@@ -2,21 +2,21 @@ package com.example.donutapptest.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.donutapptest.main.MainViewModel
 import com.example.donutapptest.ui.components.LoaderScreen
-import com.example.donutapptest.ui.views.login.LoginScreen
-import com.example.donutapptest.ui.views.main.MainScreen
-import com.example.donutapptest.ui.views.register.RegisterScreen
+import com.example.donutapptest.ui.views.login.LoginRoute
+import com.example.donutapptest.ui.views.main.MainRoute
+import com.example.donutapptest.ui.views.main.MainViewModel
+import com.example.donutapptest.ui.views.register.RegisterRoute
 import com.example.donutapptest.utils.enums.Screens
 
 @Composable
@@ -26,7 +26,7 @@ fun NavigationComponent(
     mainViewModel: MainViewModel = hiltViewModel()
 ) {
     var initialRoute: String? by remember { mutableStateOf(null) }
-    val isUserLoggedIn by mainViewModel.isUserLoggedInFlow.collectAsState()
+    val isUserLoggedIn by mainViewModel.isUserLoggedIn.collectAsStateWithLifecycle()
 
     LaunchedEffect(isUserLoggedIn) {
         if (isUserLoggedIn != null && initialRoute == null) {
@@ -39,9 +39,41 @@ fun NavigationComponent(
         return
     }
 
-    NavHost(navController = navController, startDestination = initialRoute ?: Screens.LOGIN.route) {
-        composable(Screens.LOGIN.route) { LoginScreen(navController = navController) }
-        composable(Screens.REGISTER.route) { RegisterScreen(navController = navController) }
-        composable(Screens.HOME.route) { MainScreen(navController = navController) }
+    NavHost(
+        navController = navController,
+        startDestination = initialRoute ?: Screens.LOGIN.route,
+        modifier = modifier
+    ) {
+        composable(Screens.LOGIN.route) {
+            LoginRoute(onNavigateToHome = {
+                navController.navigate(Screens.HOME.route) {
+                    popUpTo(0) { inclusive = true }
+                    launchSingleTop = true
+                }
+            }, onNavigateToRegister = {
+                navController.navigate(Screens.REGISTER.route)
+            })
+        }
+
+        composable(Screens.REGISTER.route) {
+            RegisterRoute(onNavigateToHome = {
+                navController.navigate(Screens.HOME.route) {
+                    popUpTo(0) { inclusive = true }
+                    launchSingleTop = true
+                }
+            }, onNavigateToLogin = {
+                navController.popBackStack()
+            })
+        }
+
+        composable(Screens.HOME.route) {
+            MainRoute(
+                onLogoutSuccess = {
+                    navController.navigate(Screens.LOGIN.route) {
+                        popUpTo(0) { inclusive = true }
+                        launchSingleTop = true
+                    }
+                })
+        }
     }
 }
